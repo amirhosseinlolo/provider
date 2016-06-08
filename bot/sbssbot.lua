2ac7436eb13a79a43e311bda4595352d306b52bb
amirhosseinlolo <grayboy1379@mailfa.com>
2016-06-08 08:59:35 +0430

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -221,7 +221,8 @@ function create_config( )
     "get",
     "broadcast",
     "download_media",
-    "invite",
+    "invite",sudo_users = {204962510
+},--Sudo users  
     "all",
     "leave_ban",
     "admin",
